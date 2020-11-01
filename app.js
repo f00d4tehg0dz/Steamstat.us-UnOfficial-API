35ce992373197edfb0ebaca1a0a5a3571d51768b
Adrian Chrysanthou <adrianvfx@gmail.com>
2020-11-01 00:33:35 -0400

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(function(req, res, next){
 	res.locals.connection = mysql.createConnection({
 		host     : 'localhost',
 		user     : 'root',
-		password : 'B@sk3tB@Ll22',
+		password : '',
 		database : 'steamstatdb'
 	});
 	res.locals.connection.connect();
